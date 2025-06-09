8555a4c10bdd7e6052cfc132d89c8929b576e343
Maaahmwd19 <mahmoud.ibrahim.ellaban@gmail.com>
2025-06-09 04:12:57 +0300

    web applocation struceture

---
+++ IVR-Platform/src/main/java/com/ivr/platform/utils/DBconnection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ivr.platform.utils;
+
+/**
+ *
+ * @author mibrahim
+ */
+public class DBconnection {
+    
+}
